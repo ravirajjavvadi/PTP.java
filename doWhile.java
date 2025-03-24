95a33d5ce08302f75e25b0109b4e700a62b34fcd
ravirajjavvadi <ravirajjavvadi@gmail.com>
2025-03-24 12:27:27 +0530

    Add files via upload

---
+++ doWhile.java
@@ -0,0 +1,15 @@
+class doWhiledemo{
+       public static void main(String...args){
+	        int x = 1;
+			do{
+			      if(x <= 10){
+				      System.out.println(x);
+					  x++;
+					  
+				  }else{
+				    break;
+				  }
+			}while(true);
+	   }
+
+}
