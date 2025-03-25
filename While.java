6c027c0efd4f5913e8c02bf8955581ccf6799ec1
ravirajjavvadi <ravirajjavvadi@gmail.com>
2025-03-25 09:09:34 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,15 @@
+class Whiledemo{
+       public static void main(String...args){
+	        int x = 9;
+			while(x <= 10){
+			      if(true){
+				      System.out.println(x);
+					  x++;
+					  
+				  }else{
+				      System.out.println("x is false");
+				  }
+			}
+	   }
+
+}
