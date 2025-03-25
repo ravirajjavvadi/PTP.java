98589999bb5771094a717f64b769303236fd0240
ravirajjavvadi <ravirajjavvadi@gmail.com>
2025-03-25 09:06:47 +0530

    Add files via upload

---
+++ Fordemo.java
@@ -0,0 +1,13 @@
+class Fordemo{
+       public static void main(String...args){
+	        int x = 1;
+			for(;;){
+			        if(x<=10){
+				      System.out.println(x);
+					  x++;
+					}
+					  
+				  }
+			}
+	   }
+
